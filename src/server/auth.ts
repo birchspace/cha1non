42a7904f78e5cde0a921db296d1813c47c1e554d
Birch <birch17261@gmail.com>
2023-10-20 12:15:47 +0800

    feat: test

--- src/server/auth.ts
+++ src/server/auth.ts
@@ -68,7 +68,7 @@ export const authOptions: NextAuthOptions = {
             ) as Partial<SiweMessage>,
           );
 
-          const nextAuthUrl = "https://cha1n0n.vercel.app/";
+          const nextAuthUrl = env.NEXTAUTH_URL;
 
           if (!nextAuthUrl) {
             return null;
